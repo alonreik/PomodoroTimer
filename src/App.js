5983100979a23b94d43cc61cb477ba8e3280a96f
Alon <reik@cherrypick-consulting.com>
2021-04-20 18:02:10 +0300

    commiring before the end of the day, will continue tomorrow

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 // import { render } from 'react-dom';
 
-// todo
+// A class representing a countdown clock.
 class CountdownClock extends React.Component {
 
   // constants
@@ -22,10 +22,14 @@ class CountdownClock extends React.Component {
     }
   }
 
-  // This method will be invoked every time
+  //
+  componentDidUpdate() {
+    
+  }
+
+  // This method is invoked once, and sets a timer.
   // https://betterprogramming.pub/building-a-simple-countdown-timer-with-react-4ca32763dda7
   componentDidMount() {
-
     this.myInterval = setInterval( () => {
       const { seconds, minutes } = this.state
 
@@ -34,15 +38,16 @@ class CountdownClock extends React.Component {
          return
       }
 
-      if (seconds > 0 && !this.state.isPaused) {
+      if (seconds > 0) {
           this.setState(({seconds}) => ({
             seconds: seconds - 1
           }))
       } else if (seconds === 0) {
           if (minutes === 0) { // if time is up
-            clearInterval(this.myInterval)
+            this.resetTimer()
+            this.props.applyWhenDone();
           } else { // minutes > 0
-            this.setState(({minutes}) => ({
+            this.setState( ({minutes}) => ({
               minutes: minutes - 1,
               seconds: 59
             }))
@@ -56,11 +61,12 @@ class CountdownClock extends React.Component {
     this.state.isPaused ? this.setState({isPaused:false}) : this.setState({isPaused:true})
   }
 
+  //
   resetTimer() {
     this.setState({
       minutes: this.initialMinutesCounter,
       seconds: this.initialSecondsCounter,
-      isPaused:true
+      isPaused: true
     })
   }
 
@@ -71,7 +77,7 @@ class CountdownClock extends React.Component {
     // displaying the data (notice the usage of string literals).
     return (
       <div>
-        {minutes === 0 && seconds === 0 ?
+        { minutes === 0 && seconds === 0 ?
         <h1> Time is up </h1>:
         <h1> Time Remaining for X is { minutes }:{ seconds < 10 ? `0${ seconds }`: seconds} </h1>
         }
@@ -85,32 +91,52 @@ class CountdownClock extends React.Component {
 }
 
 //
-class PomodotoTimer extends React.Component {
+class PomodoroTimer extends React.Component {
 
+  // Pomodoro set : 25:00
+  // break set: 5:00
+
+  //
   constructor(props) {
     super(props)
     this.state = {
-
+      // Setting initial times
+      minutesForTimer: 0,
+      secondsForTimer: 4,
+      isOnBreak: false,
+      pomodoroSetsCounter: 0
     }
+
+    // bindind the "this"-term in "applyWhenTimerIsDone" to this (PomodoroTimer)
+    this.applyWhenTimerIsDone = this.applyWhenTimerIsDone.bind(this)
+  }
+
+  //
+  applyWhenTimerIsDone() {
+    this.setState(() => ({
+      isOnBreak: !this.state.isOnBreak,
+      minutesForTimer: this.state.isOnBreak ? 0 : 0,
+      secondsForTimer: this.state.isOnBreak ? 2: 3,
+      pomodoroSetsCounter: this.state.pomodoroSetsCounter + 1,
+    }))
   }
 
   // todo
   render() {
     return (
       <div>
-      <h3> Welcome to PomodotoTimer </h3>
-
+        <h1> pomodoro set counter: {this.state.pomodoroSetsCounter} </h1>
+        <h3> Welcome to PomodoroTimer </h3>
         <h4>
         <CountdownClock
-        minutes={0}
-        seconds={10}
-        />
+          minutes={this.state.minutesForTimer}
+          seconds={this.state.secondsForTimer}
+          applyWhenDone={()=>{this.applyWhenTimerIsDone()}}
+          />
         </h4>
       </div>
     )
   }
 }
 
-
-
-export default PomodotoTimer;
+export default PomodoroTimer;
